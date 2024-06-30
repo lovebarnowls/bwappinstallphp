36bfc01da75b67c312421adf1637abe938c1d87b
lovebarnowls <114584264+lovebarnowls@users.noreply.github.com>
2024-06-30 15:27:06 +1000

    Update install.php

--- install.php
+++ install.php
@@ -106,3 +106,65 @@ $link->close();
 
 echo "Database setup completed.";
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<!--<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Architects+Daughter">-->
+<link rel="stylesheet" type="text/css" href="stylesheets/stylesheet.css" media="screen" />
+<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+<!--<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>-->
+<script src="js/html5.js"></script>
+<title>bWAPP - Installation</title>
+</head>
+<body>
+<header>
+<h1>bWAPP</h1>
+<h2>an extremely buggy web app !</h2>
+</header>
+<div id="menu">
+    <table>
+        <tr>
+        <?php
+        if($db == 1)
+        {
+        ?>
+            <td><a href="login.php">Login</a></td>
+            <td><a href="user_new.php">New User</a></td>
+            <td><a href="info.php">Info</a></td>
+            <td><a href="training.php">Talks & Training</a></td>
+            <td><a href="http://itsecgames.blogspot.com" target="_blank">Blog</a></td>
+        <?php
+        }
+        else
+        {
+        ?>
+            <td><font color="#ffb717">Install</font></td>
+            <td><a href="info_install.php">Info</a></td>
+            <td><a href="training_install.php">Talks & Training</a></td>
+            <td><a href="http://itsecgames.blogspot.com" target="_blank">Blog</a></td>
+        <?php
+        }
+        ?>
+        </tr>
+    </table>
+</div>
+<div id="main">
+    <h1>Installation</h1>
+    <p><?php echo $message?></p>
+</div>
+<div id="side">
+    <a href="http://twitter.com/MME_IT" target="blank_" class="button"><img src="./images/twitter.png"></a>
+    <a href="http://be.linkedin.com/in/malikmesellem" target="blank_" class="button"><img src="./images/linkedin.png"></a>
+    <a href="http://www.facebook.com/pages/MME-IT-Audits-Security/104153019664877" target="blank_" class="button"><img src="./images/facebook.png"></a>
+    <a href="http://itsecgames.blogspot.com" target="blank_" class="button"><img src="./images/blogger.png"></a>
+</div>
+<div id="disclaimer">
+    <p>bWAPP is licensed under <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/" target="_blank"><img style="vertical-align:middle" src="./images/cc.png"></a> &copy; 2014 MME BVBA / Follow <a href="http://twitter.com/MME_IT" target="_blank">@MME_IT</a> on Twitter and ask for our cheat sheet, containing all solutions! / Need an exclusive <a href="http://www.mmebvba.com" target="_blank">training</a>?</p>
+</div>
+<div id="bee">
+    <img src="./images/bee_1.png">
+</div>
+</body>
+</html>
